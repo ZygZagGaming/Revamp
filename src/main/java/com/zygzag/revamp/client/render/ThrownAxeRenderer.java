7a39d69689274063f4ef5d400de2617b084065de
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2022-01-25 21:25:31 -0500

    p

--- src/main/java/com/zygzag/revamp/client/render/ThrownAxeRenderer.java
+++ src/main/java/com/zygzag/revamp/client/render/ThrownAxeRenderer.java
@@ -1,42 +1,16 @@
 package com.zygzag.revamp.client.render;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Vector3f;
 import com.zygzag.revamp.common.entity.ThrownAxe;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-public class ThrownAxeRenderer extends EntityRenderer<ThrownAxe> {
-    public static final ResourceLocation LOC = new ResourceLocation("revamp:items/wither_skull_socketed_iridium_axe");
-    private final ItemRenderer itemRenderer;
-
+public class ThrownAxeRenderer extends ThrownItemRenderer<ThrownAxe> {
     public ThrownAxeRenderer(EntityRendererProvider.Context context) {
         super(context);
-        itemRenderer = context.getItemRenderer();
-    }
-
-    public void render(ThrownAxe axe, float a, float b, PoseStack stack, MultiBufferSource source, int shadowRadius) {
-        stack.pushPose();
-        stack.mulPose(this.entityRenderDispatcher.cameraOrientation());
-        stack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
-        this.itemRenderer.renderStatic(axe.getItem(), ItemTransforms.TransformType.GROUND, shadowRadius, OverlayTexture.NO_OVERLAY, stack, source, axe.getId());
-        stack.popPose();
-        super.render(axe, a, b, stack, source, shadowRadius);
-    }
-
-    @Override
-    public ResourceLocation getTextureLocation(ThrownAxe axe) {
-        return LOC;
     }
 }
--- src/main/java/com/zygzag/revamp/common/entity/ThrownAxe.java
+++ src/main/java/com/zygzag/revamp/common/entity/ThrownAxe.java
@@ -1,30 +1,37 @@
 package com.zygzag.revamp.common.entity;
 
 import com.zygzag.revamp.common.registry.Registry;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ItemSupplier;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 
-public class ThrownAxe extends ThrownTrident {
-    private ItemStack item;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class ThrownAxe extends ThrownTrident implements ItemSupplier {
+    private final ItemStack item;
     public ThrownAxe(EntityType<? extends ThrownAxe> type, Level world) {
         super(type, world);
         this.item = ItemStack.EMPTY;
     }
 
     public ThrownAxe(Level world, Player owner, ItemStack item) {
-        super(Registry.THROWN_AXE.get(), world);
+        super(Registry.THROWN_AX
         this.setOwner(owner);
         this.pickup = AbstractArrow.Pickup.ALLOWED;
         this.setPos(owner.getX(), owner.getEyeY() - 0.1f, owner.getZ());
-        this.item = item;
+        System.out.println(item);
+        this.item = item.copy();
     }
 
     public ItemStack getItem() {
+        System.out.println(item);
         return item;
     }
 }
