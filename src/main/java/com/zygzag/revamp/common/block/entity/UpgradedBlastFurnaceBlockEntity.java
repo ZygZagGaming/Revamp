547ea95eba9d16f29eb9a912413b11b1ab3a747b
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2022-02-11 14:37:07 -0500

    bogus amogus

--- src/main/java/com/zygzag/revamp/common/block/entity/UpgradedBlastFurnaceBlockEntity.java
+++ src/main/java/com/zygzag/revamp/common/block/entity/UpgradedBlastFurnaceBlockEntity.java
@@ -2,6 +2,7 @@ package com.zygzag.revamp.common.block.entity;
 
 import com.zygzag.revamp.common.block.menu.UpgradedBlastFurnaceMenu;
 import com.zygzag.revamp.common.item.recipe.ModRecipeType;
+import com.zygzag.revamp.common.item.recipe.UpgradedBlastFurnaceRecipe;
 import com.zygzag.revamp.common.registry.Registry;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
@@ -11,9 +12,11 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @MethodsReturnNonnullByDefault
@@ -39,5 +42,17 @@ public class UpgradedBlastFurnaceBlockEntity extends AbstractFurnaceBlockEntity 
         return super.getBurnDuration(stack) / 2;
     }
 
-
+    @Override
+    public boolean burn(@Nullable Recipe<?> recipe, NonNullList<ItemStack> items, int time) {
+        /*  items[0] = input
+            items[1] = fuel
+            items[2] = output   */
+        if (canBurn(recipe, items, time) && recipe instanceof UpgradedBlastFurnaceRecipe uRecipe && uRecipe.getChance() < Math.random()) {
+            ItemStack extraResult = uRecipe.getOutSecondary().copy();
+            ItemStack current = getItem(3);
+            if (current.isEmpty()) setItem(3, extraResult);
+            else if (current.getItem() == extraResult.getItem() && current.getCount() + extraResult.getCount() <= getMaxStackSize()) current.grow(extraResult.getCount());
+        }
+        return super.burn(recipe, items, time);
+    }
 }
--- src/main/java/com/zygzag/revamp/common/block/menu/UpgradedBlastFurnaceMenu.java
+++ src/main/java/com/zygzag/revamp/common/block/menu/UpgradedBlastFurnaceMenu.java
@@ -2,22 +2,76 @@ package com.zygzag.revamp.common.block.menu;
 
 import com.zygzag.revamp.common.item.recipe.ModRecipeType;
 import com.zygzag.revamp.common.registry.Registry;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
-import net.minecraft.world.item.crafting.AbstractCookingRecipe;
-import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.ItemStack;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class UpgradedBlastFurnaceMenu extends AbstractFurnaceMenu {
     public static final int SECONDARY_RESULT_SLOT = 3;
     public UpgradedBlastFurnaceMenu(int id, Inventory inventory) {
         super(Registry.UPGRADED_BLAST_FURNACE_MENU.get(), ModRecipeType.UPGRADED_BLASTING, RecipeBookType.BLAST_FURNACE, id, inventory);
-        this.addSlot(new FurnaceResultSlot(inventory.player, new SimpleContainer(4), 3, 116, 75));
+        this.addSlot(new FurnaceResultSlot(inventory.player, new SimpleContainer(4), 3, 156, 75));
     }
 
     public UpgradedBlastFurnaceMenu(int id, Inventory inventory, Container container, ContainerData data) {
         super(Registry.UPGRADED_BLAST_FURNACE_MENU.get(), ModRecipeType.UPGRADED_BLASTING, RecipeBookType.BLAST_FURNACE, id, inventory, container, data);
-        this.addSlot(new FurnaceResultSlot(inventory.player, container, 3, 116, 75));
+        this.addSlot(new FurnaceResultSlot(inventory.player, container, 3, 156, 75));
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+        if (slot.hasItem()) {
+            ItemStack itemstack1 = slot.getItem();
+            itemstack = itemstack1.copy();
+            if (index == 2 || index == 3) {
+                if (!this.moveItemStackTo(itemstack1, 3, 39, true)) {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onQuickCraft(itemstack1, itemstack);
+            } else if (index != 1 && index != 0) {
+                if (this.canSmelt(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.isFuel(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 1, 2, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (index >= 3 && index < 30) {
+                    if (!this.moveItemStackTo(itemstack1, 30, 39, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (index >= 30 && index < 39 && !this.moveItemStackTo(itemstack1, 3, 30, false)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.moveItemStackTo(itemstack1, 3, 39, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.set(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(player, itemstack1);
+        }
+
+        return itemstack;
     }
 }
